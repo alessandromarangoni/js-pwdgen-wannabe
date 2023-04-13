45948c1afb2aa472e2319948e011b741430e58f1
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-04-13 09:49:14 +0200

    dimenticato il trattino tra gli elementi

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let yourSurname = prompt("inserisci il tuo cognome");
 let favouriteColor = prompt("inserisci il tuo colore perferito");
 let currentYear = "2023";
 
-let yourPassword = `la tua password è ${yourName}${yourSurname}${favouriteColor}${currentYear}`;
+let yourPassword = `la tua password è ${yourName}-${yourSurname}-${favouriteColor}-${currentYear}`;
 
 document.getElementById("selezione").innerText = yourPassword ;
+
